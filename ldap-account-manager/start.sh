08984cd26496350fe7077dc39b61ebc7d328b01a
trydofor <trydofor@gmail.com>
2018-04-08 17:42:05 +0800

    lam update

--- ldap-account-manager/start.sh
+++ ldap-account-manager/start.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-if [ ! -e /config/config.cfg ]; then
-  /bin/cp -f /var/lib/ldap-account-manager/config/config.cfg.sample /config/config.cfg
-  chown www-data:www-data -R /config/config.cfg
-  chown www-data:www-data -R /var/lib/ldap-account-manager/config/config.cfg
-
+if [ ! -e /config/config ]; then
+  cp -rf /var/lib/ldap-account-manager/config.back /config/config
 fi
 
 if [ ! -e /config/sess ]; then
-  mkdir /config/mess
-  /bin/cp -rf /var/lib/ldap-account-manager/sess.back /config/sess
-  chown www-data:www-data -R /config/sess
-  chmod www-data:www-data -R /var/lib/ldap-account-manager/sess
+  cp -rf /var/lib/ldap-account-manager/sess.back /config/sess
 fi
 
+rm -rf /var/lib/ldap-account-manager/config
+rm -rf /var/lib/ldap-account-manager/sess
+rm -rf /var/log/apache2/*
+rm -rf /var/run/apache2/apache2.pid
 
-rm -f /var/run/apache2/apache2.pid
+chown www-data:www-data -R /config/
+
+ln -sf /config/config  /var/lib/ldap-account-manager/config
+ln -sf /config/sess    /var/lib/ldap-account-manager/sess
+ln -sf /dev/stdout     /var/log/apache2/access.log
+ln -sf /dev/stderr     /var/log/apache2/error.log
+
 /usr/sbin/apache2ctl -D FOREGROUND
